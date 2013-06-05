33c84866ae2ed667576790c7f662821d70f1a284
Cary Corse <ccorse@gmail.com>
2013-06-05 10:38:21 +0100

    Added website files

---
+++ html_stuff/js/config.js
@@ -0,0 +1,26 @@
+
+var config = {
+
+	navpatterns : {
+		
+		// Responsive Nav JavaScript plugin
+		// http://www.smashingmagazine.com/2013/04/09/responsive-nav-a-simple-javascript-plugin-for-responsive-navigation/
+		responsivenav : {
+			use 		: true,
+			navelement  : "#navigation-toggle"
+		}
+
+	},
+	widgets : {
+
+		tooltips : {
+			use 		: true,
+			container	: ".tooltips"
+		},
+
+		popovers : {
+			use 		: true
+		}
+
+	}
+};
